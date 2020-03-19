fa236ac93f98a17863d82b54c3dbf5e00bbb5f39
Shelly <shelly980102@gmail.com>
2020-03-19 13:53:21 +0800

    Initial Commit

---
+++ avatar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  avatar
+//
+//  Created by 蕭聿莘 on 2020/3/19.
+//  Copyright © 2020 Kazuku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
